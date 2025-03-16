fc62cafe0655bb345c606163880bb452f897c9b4
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2025-03-16 14:49:16 +0100

    I have now provided a pseudo code for the Alorithm

--- tutorial_course2/src/main/java/eleve/hhamnnes/tutorial2/Main.java
+++ tutorial_course2/src/main/java/eleve/hhamnnes/tutorial2/Main.java
@@ -1,8 +1,20 @@
 package eleve.hhamnnes.tutorial2;
 
+import eleve.hhamnnes.tutorial2.first_part.MergeSortRecursiveAlgorithm;
+
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer[] list = {1,5,3,40, 2, 9};
+
+        MergeSortRecursiveAlgorithm mergeSortRecursiveAlgorithm = new MergeSortRecursiveAlgorithm();
+
+        list = mergeSortRecursiveAlgorithm.execute(list);
+
+        for (int i = 0; i < list.length; i ++){
+            System.out.println(list[i]);
+        }
     }
 }
