0a378ce7e9aafc628a4836de097b3244dab8800b
hhamnnes <96078953+hhamnnes@users.noreply.github.com>
2025-03-16 17:25:42 +0100

    I have now implemented a interative code for the mergeSort algortihm

--- tutorial_course2/src/main/java/eleve/hhamnnes/tutorial2/Main.java
+++ tutorial_course2/src/main/java/eleve/hhamnnes/tutorial2/Main.java
@@ -1,13 +1,13 @@
 package eleve.hhamnnes.tutorial2;
 
 import eleve.hhamnnes.tutorial2.first_part.MergeSortRecursiveAlgorithm;
-
+import eleve.hhamnnes.tutorial2.first_part.MergeSortIterativeAlgorithm;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] list = {1,5,3,40, 2, 9};
+        Integer[] list = {6, 5, 3, 1, 8, 7, 2, 4};
 
         MergeSortRecursiveAlgorithm mergeSortRecursiveAlgorithm = new MergeSortRecursiveAlgorithm();
 
@@ -16,5 +16,22 @@ public class Main {
         for (int i = 0; i < list.length; i ++){
             System.out.println(list[i]);
         }
+
+        Integer[] list2 = {6, 5, 3, 1, 8, 7, 2, 4};
+
+        MergeSortIterativeAlgorithm mergeSortIterativeAlgorithm = new MergeSortIterativeAlgorithm();
+
+        list2 = mergeSortIterativeAlgorithm.execute(list2);
+
+        for (int i = 0; i < list.length; i ++){
+            System.out.println(list2[i]);
+        }
+
+
+
+
+        
+
+
     }
 }
---
+++ tutorial_course2/src/main/java/eleve/hhamnnes/tutorial2/first_part/MergeSortIterativeAlgorithm.java
@@ -0,0 +1,52 @@
+package eleve.hhamnnes.tutorial2.first_part;
+
+import eleve.hhamnnes.tutorial2.interfaces.MergeSortAlgorithm;
+
+public class MergeSortIterativeAlgorithm implements MergeSortAlgorithm {
+
+    @Override
+    public Integer[] execute(Integer[] list) {
+        if (list.length <= 1) {
+            return list;
+        }
+
+        Integer[] temp = new Integer[list.length];
+
+        for (int width = 1; width < list.length; width *= 2) {
+            for (int i = 0; i < list.length; i += 2 * width) {
+                int left = i;
+                int middle = Math.min(i + width, list.length);
+                int right = Math.min(i + 2 * width, list.length);
+                merge(list, temp, left, middle, right);
+            }
+        }
+
+        return list;
+    }
+
+    private void merge(Integer[] list, Integer[] temp, int left, int middle, int right) {
+        int i = left;
+        int j = middle;
+        int k = left;
+
+        while (i < middle && j < right) {
+            if (list[i] <= list[j]) {
+                temp[k++] = list[i++];
+            } else {
+                temp[k++] = list[j++];
+            }
+        }
+
+        while (i < middle) {
+            temp[k++] = list[i++];
+        }
+
+        while (j < right) {
+            temp[k++] = list[j++];
+        }
+
+        for (i = left; i < right; i++) {
+            list[i] = temp[i];
+        }
+    }
+}
